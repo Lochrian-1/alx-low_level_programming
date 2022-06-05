a0a927e5ad8343bb3f348e0bdfac08c03c50b61a
Maxwell Marks <104769281+Lochrian-1@users.noreply.github.com>
2022-06-05 21:37:19 +0200

    task7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
 
- /**
-  * main - prints lowercase letters of the alphabet
-  *
-  * Description: prints letters of alphabet in reverse
-  *
-  * Return: 0 when successful
-  */
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: print the letters of the alphabet in reverse
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	int i;
-
-	i = 122;
+	int i = 122;
 
 	while (i > 96)
 	{
 		putchar(i);
 		i--;
 	}
-
 	putchar(10);
 
 	return (0);
