414cd9760931c9afc28382abe273037070f393b7
Max <workbusmax@gmail.com>
2022-06-05 12:46:20 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints all numbers of base 16
+ *
+ * Description: prints all numbers of base 16 in lowercase
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	int i;
+	i = 0;
+
+	while (i < 48)
+	{
+		if (i < 10)
+			putchar(1 + '0');
+		else if (i > 41)
+			putchar(i -10 + 'A');
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
