89cda84cb3c13a4c312245562861eb0671ce0a83
Max <workbusmax@gmail.com>
2022-06-05 11:47:50 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit numbers
+ *
+ * Description: print all single digit numbers of base 10
+ * starting with 0
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar(10);
+	
+	return (0);
+}
