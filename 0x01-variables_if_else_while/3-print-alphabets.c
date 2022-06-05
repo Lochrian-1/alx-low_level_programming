3314ae940be9aee337819a5e6483ee8e14eea9d5
Max <workbusmax@gmail.com>
2022-06-05 11:16:46 -0700

    task3

---
+++ 0x01-variables_if_else_while/3-print-alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints all letters of the alphabet
+ *
+ * Description: print all letters in both upper and lowercase
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int i, j;
+
+	i = 97;
+	j = 65;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+
+	while (j < 91)
+	{
+		putchar(j);
+		j++;
+	}
+	putchar(10);
+
+	return(0);
+}
