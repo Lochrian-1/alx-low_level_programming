b2fd311c52e96b3e26ff9aeeeb07aae375949834
Max <workbusmax@gmail.com>
2022-06-07 13:17:08 -0700

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,15 +2,14 @@
 
 /**
  * _islower - checks for lowercase characters
+ * @c: c is an ascii character
  *
  * Descriptionn: checks for lowercase characters
  *
  * Return: 1 when c is a lowercase and 0 when otherwise
  */
 
-int _islower(void)
-{
-	int c;
+int _islower(int c)
 
 	if (c >= 'a' && c <= 'z')
 	{
