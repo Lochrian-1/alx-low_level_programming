633f176770c83c87303556712be64944c1fe9d56
Max <workbusmax@gmail.com>
2022-06-07 12:59:54 -0700

    task3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase characters
+ *
+ * Descriptionn: checks for lowercase characters
+ *
+ * Return: 1 when c is a lowercase and 0 when otherwise
+ */
+
+int _islower(void)
+{
+	int c;
+
+	if (_islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
