1e4fb8f3b6efc299809174c6d0dd40e3172a2f49
Max <workbusmax@gmail.com>
2022-06-07 13:35:11 -0700

    task5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: n is an ascii character
+ *
+ * Description: prints the sign for a number
+ *
+ * Return: 1 if positice, 0 if zero and -1 if negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else
+	{
+		putchar('-1');
+		return (-1);
+	}
+}
