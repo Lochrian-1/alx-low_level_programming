0c4fd9f934fda6a58fce93b4d1d37ddf39df9dc6
Max <workbusmax@gmail.com>
2022-06-07 12:18:28 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Description: prints alphabet 10 times in lowercase
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char lines = 0, letters;
+
+	while(lines <= 9)
+	{
+		letters = 'a';
+		while(letters <= 'z')
+		{
+			_putchar(letters);
+			letters++;
+		}
+		_putchar('\n');
+		lines++;
+	}
+}
