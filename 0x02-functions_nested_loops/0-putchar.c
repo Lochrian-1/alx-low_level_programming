47badc68cdd3f962d910a732913b503890ef05ab
Max <workbusmax@gmail.com>
2022-06-07 09:42:40 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar
+ *
+ * Description: prints _putchar followed by a new line
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	char text[10] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+	{
+		printf(text[i]);
+	}
+	printf(\n);
+
+	return (0);
+}
