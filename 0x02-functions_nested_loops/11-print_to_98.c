98772c8cd37a37517d970d1f223110186e2e1d80
Max <workbusmax@gmail.com>
2022-06-08 06:45:14 -0700

    task11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers to 98
+ * @n: n is an interger
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	for (n < "98")
+	{
+		while (n != "98")
+		{
+			_putchar(n);
+			for (n != "98")
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+			n++;
+		}
+	}
+	else if (n > "98")
+	{
+		while (n != "98")
+		{
+			_putchar(n);
+			for (n != "98")
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n--;
+		}
+	}
+	else
+	{
+		_putchar("98");
+	}
+}
