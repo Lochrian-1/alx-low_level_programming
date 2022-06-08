98899703ec0afc5f54540f61c9d7a5fd8602af4d
Max <workbusmax@gmail.com>
2022-06-08 07:17:15 -0700

    task11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,18 +17,15 @@ void print_to_98(int n)
 			printf("%d, ", n);
 			n++;
 		}
+		printf("98\n");
 	}
-	else if (n > 98)
+	else
 	{
 		while (n != 98)
 		{
 			printf("%d, ", n);
 			n--;
 		}
+		printf("98\n");
 	}
-	else
-	{
-		printf("%d", 98);
-	}
-	_putchar('\n');
 }
