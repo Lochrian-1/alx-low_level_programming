1f8e7bac1f18ede9df2cdf8badc06f705a977404
Max <workbusmax@gmail.com>
2022-06-08 04:15:28 -0700

    task9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Description: prints the 9 times table, starting from 0
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int R, C, product, tens, units;
+	int R = 0;
+
+	while (R <= 9)
+	{
+		C = 0;
+		while (C <= 9)
+		{
+			product = R * C;
+			tens = product / 10;
+			units = product % 10;
+
+			if (C == 0)
+			{
+				_putchar(0);
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar('  ');
+				_putchar(units + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(units + '0');
+			}
+			C++;
+		}
+		R++;
+	}
+	_putchar('\n');
+}
