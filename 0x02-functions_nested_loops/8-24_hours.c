1e35188dcbdaf311ec1ed81742dfed5ef43874b1
Max <workbusmax@gmail.com>
2022-06-08 00:27:41 -0700

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,7 +3,8 @@
 /**
  * jack_bauer -prints every minute of the day of Jack Bauer
  *
- * Description:prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ * Description:prints every minute of the day of Jack Bauer,
+ *  starting from 00:00 to 23:59
  *
  * Return: void
  */
@@ -13,14 +14,14 @@ void jack_bauer(void)
 	int H = 0;
 	int M = 0;
 
-	while (hours <= 23)
+	while (H <= 23)
 	{
-		while (minutes <= 59)
+		while (M <= 59)
 		{
-			_putchar("%02d:%02d\n", hours, minutes);
-			minutes++;
+			_putchar("%02d:%02d\n", H, M);
+			M++;
 		}
-		hours++;
-		minutes = 0
-	}	
+		H++;
+		M = 0;
+	}
 }
