295a4a5aa40c0c4953db847f76c6a1ad0f2c64ad
Max <workbusmax@gmail.com>
2022-06-08 06:22:54 -0700

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two intergers
+ * 
+ * Description: adds two intergers and returns the sum of it
+ *
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+	result = a + b;
+	return (result);
+}
