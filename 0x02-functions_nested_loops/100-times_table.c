85ffd909d5695043ddfa108932a5316a8158611c
Max <workbusmax@gmail.com>
2022-06-08 07:54:07 -0700

    task12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,18 +11,19 @@
 
 void print_times_table(int n)
 {
-	int R, C, product, tens, units;
+	int R, C, product, hundreds, tens, units;
 
 	R = 0;
-	
+
 	if (n >= 0 && n <= 15)
 	{
-		while (R <= n)
+		while (n >= R)
 		{
 			C = 0;
-			while (C <= n)
+			while (n >= C)
 			{
 				product = R * C;
+				hundreds = product / 100;
 				tens = product / 10;
 				units = product % 10;
 				if (C == 0)
@@ -34,13 +35,23 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar(' ');
+					_putchar(units + '0');
+				}
+				else if (product > 10 && product < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((tens % 10) + '0');
 					_putchar(units + '0');
 				}
 				else
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(tens + '0');
+					_putchar(hundreds + '0');
+					_putchar((tens % 10) + '0');
 					_putchar(units + '0');
 				}
 				C++;
