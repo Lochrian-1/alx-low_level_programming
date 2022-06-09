dd00405838960e356fcf8d2604749902eb5d5fdb
Max <workbusmax@gmail.com>
2022-06-09 15:36:06 -0700

    taskr

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiples the two intergers
+ * @a: is an interger
+ * @b: is an interger
+ *
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = (a * b);
+	return (product);
+}
