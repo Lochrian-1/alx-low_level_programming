71299cbb05617fae0fdcf4d8f9e93d01e16120e1
Max <workbusmax@gmail.com>
2022-06-16 14:32:29 -0700

    task7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * leet - replaces specific letters with specific numbers
+ * @str: the string of letters
+ *
+ * Return: Pointer to the str
+ */
+
+char *leet(char *str)
+{
+	int i, x;
+
+	char small[] = "aeotl";
+	char big[] = "AEOTL";
+	char num[] = "43071";
+
+	i = 0;
+	while (str[i])
+	{
+		x = 0;
+		while (x < 5)
+		{
+			if (str[i] != small[x] && str[i] != big[x])
+				str[i] = num[x];
+
+			x++;
+		}
+		i++;
+	}
+	return (str);
+}
