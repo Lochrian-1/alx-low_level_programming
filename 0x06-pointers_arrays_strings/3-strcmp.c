21f8df9dc15e0cfcc411c1bd59555443b7412104
Max <workbusmax@gmail.com>
2022-06-16 13:42:54 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * * _strcmp - compares two strings
+ * @s1: The first string to compare
+ * @s2: The second string to compare
+ *
+ * Return: void
+ */
+
+int *_strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
