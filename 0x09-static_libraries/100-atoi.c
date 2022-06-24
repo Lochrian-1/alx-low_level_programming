3afa77b2a72c05af8f3fba5e0b3f2c548bb9e588
Maxwell Marks <104769281+Lochrian-1@users.noreply.github.com>
2022-06-24 20:55:08 +0200

    task0

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,45 +1,29 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
+ * _atoi - convert a string to an integer.
+ * @s: char type string
  *
- * Return: the int converted from the string
+ * Return: void
  */
 
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+	int sign = 1, resp = 0, firstNum;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
+	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
 	{
-		if (s[i] == '-')
-			++d;
-		if (s[i] >= '0' && s[i] <= '9')
+		if (s[firstNum] == '-')
 		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
+			sign *= -1;
 		}
-		i++;
 	}
-	if (f == 0)
-		return (0);
 
-	return (n);
+	for (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+	{
+		resp *= 10;
+		resp += (s[i] - 48);
+	}
+
+	return (sign * resp);
 }
