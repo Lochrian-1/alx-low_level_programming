a5b6e2e29bc94260cdd0ffbe973830cce13fae6d
Lochrian-1 <workbusmax@gmail.com>
2022-06-28 16:43:01 +0200

    task4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - function to free memory from grid
+ * @grid: grid to free;
+ * @height: height of grid;
+ *
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	while (i < height)
+	{
+		free(grid[i]);
+		free(grid);
+		i++;
+	}
+}
