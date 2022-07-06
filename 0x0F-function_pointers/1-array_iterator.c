78b42293d37f7932deb38a1f85e936efa6861346
Lochrian-1 <workbusmax@gmail.com>
2022-07-07 01:28:38 +0200

    task1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes funct
+ * @array: array of elements
+ * @size: array's size
+ * @action: pointer to function
+ *
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int x;
+
+	if (array && size && action)
+	{
+		x = 0;
+		while (x < size)
+		{
+			(*action)(array[i]);
+			x++;
+		}
+	}
+}
