4dd04598ac0e0c9beec9257241321c783b0d5dd0
Lochrian-1 <workbusmax@gmail.com>
2022-07-18 12:22:52 +0200

    task0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints elements of listint_t
+ * @h: head of linked list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t s;
+
+	for (s = 0; h != NULL; s++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (size);
+}
