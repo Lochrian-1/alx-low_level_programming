7d4a7eb7cd3fe8a1eb2f8d046ccd4f20598c3f28
Lochrian-1 <workbusmax@gmail.com>
2022-07-25 19:58:32 +0200

    task3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,21 +15,13 @@ void error_messages(int exit_code, char *err_msg, char type, ...)
 
 	va_start(ar, type);
 	if (type == 's')
-	{
 		dprintf(STDERR_FILENO, err_msg, va_arg(ar, char *));
-	}
 	else if (type == 'd')
-	{
 		dprintf(STDERR_FILENO, err_msg, va_arg(ar, int));
-	}
 	else if (type == 'N')
-	{
 		dprintf(STDERR_FILENO, err_msg, "");
-	}
 	else
-	{
 		dprintf(STDERR_FILENO, "Error: Invalid type");
-	}
 
 	va_end(ar);
 	exit(exit_code);
@@ -40,48 +32,40 @@ void error_messages(int exit_code, char *err_msg, char type, ...)
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: void
+ * Return: Always 0 (success)
  */
 
 int main(int argc, char *argv[])
 {
-	int x, y;
-	char buff[1024];
-	ssize_t x_read, y_write;
+	int fd1, fd2;
+	char buf[1024];
+	ssize_t fd1_read, fd2_write;
 
 	if (argc != 3)
-	{
 		error_messages(97, "Usage: cp file_from file_to\n", 'N');
-	}
-	x = open(argv[1], O_RDONLY);
-	if (x == -1)
-	{
+
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
 		error_messages(98, "Error: Can't read from file %s\n", 's', argv[1]);
-	}
-	y = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (y == -1)
-	{
+
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)
 		error_messages(99, "Error: Can't write to %s\n", 's', argv[2]);
-	}
-	while ((x_read = read(x, buff, 1024)) > 0)
+
+	while ((fd1_read = read(fd1, buf, 1024)) > 0)
 	{
-		y_write = write(y, buff, x_read);
-		if (y_write == -1)
-		{
+		fd2_write = write(fd2, buf, fd1_read);
+		if (fd2_write == -1)
 			error_messages(99, "Error: Can't write to %s\n", 's', argv[2]);
-		}
 	}
-	if (x_read == -1)
-	{
+
+	if (fd1_read == -1)
 		error_messages(98, "Error: Can't read from file %s\n", 's', argv[1]);
-	}
-	if (close(x) == -1)
-	{
-		error_messages(100, "Error: Can't close fd %d\n", 'd', x);
-	}
-	if (close(y) == -1)
-	{
-		error_messages(100, "Error: Can't clode fd %d\n", 'd', y);
-	}
+
+	if (close(fd1) == -1)
+		error_messages(100, "Error: Can't close fd %d\n", 'd', fd1);
+	if (close(fd2) == -1)
+		error_messages(100, "Error: Can't clode fd %d\n", 'd', fd2);
+
 	return (0);
 }
